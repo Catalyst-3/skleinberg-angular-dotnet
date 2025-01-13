8238956edb6a5b41d11e94c4e2fcd772cdf187d1
stevenkleinberg <stevenkleinberg5@gmail.com>
2025-01-13 13:21:06 -0600

    skeleton ASP.net API working

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using backend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt => 
+{    
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
