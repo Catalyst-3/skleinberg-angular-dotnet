a74fc03c52d481fbae983911f8b4ec166d62cf22
stevenkleinberg <94195000+stevenkleinberg@users.noreply.github.com>
2025-01-15 13:38:36 -0600

    added http request to get todo items list and display it  (#3)
    * added http request to get todo items list and display it on the web page. also added bootstrap and font-awesome
    
    * upgraded from .net8 to .net9

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
   title = "Steven's Todo App";
+  todos: any;
+  
+  ngOnInit(): void {
+    this.http.get('http://localhost:5000/api/todo').subscribe({
+      next: response => this.todos = response,
+      error: error  => console.log(error),
+      complete: () => console.log('Request has completed')
+    })
+  }
+  
+
 }
