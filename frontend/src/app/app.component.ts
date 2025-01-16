b0944ec0a047278d489a6fa65c3f539c9d839a8c
stevenkleinberg <stevenkleinberg5@gmail.com>
2025-01-16 16:05:46 -0600

    frontend API_URL loaded dynamically through env vars

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +12,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   http = inject(HttpClient);
+  apiUrl = environment.apiUrl;
   title = "Steven's Todo App";
   todos: any;
   
   ngOnInit(): void {
-    this.http.get('http://localhost:8080/api/todo').subscribe({
+    this.http.get(this.apiUrl + "/api/todo").subscribe({
       next: response => this.todos = response,
       error: error  => console.log(error),
       complete: () => console.log('Request has completed')
