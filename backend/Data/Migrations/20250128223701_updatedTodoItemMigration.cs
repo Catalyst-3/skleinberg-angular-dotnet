77643c8d6c6b9cf25666f5fc02dfc536397f3baf
stevenkleinberg <94195000+stevenkleinberg@users.noreply.github.com>
2025-01-29 10:33:55 -0600

    Updated todo entity and migration (#12)
    * updated todoitem entity and created new migration and applied it to fresh database
    
    * Remove packages-microsoft-prod.deb from tracking

---
+++ backend/Data/Migrations/20250128223701_updatedTodoItemMigration.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedTodoItemMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "TodoItems",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsComplete",
+                table: "TodoItems",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "TodoItems",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Updated",
+                table: "TodoItems",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "TodoItems");
+
+            migrationBuilder.DropColumn(
+                name: "IsComplete",
+                table: "TodoItems");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "TodoItems");
+
+            migrationBuilder.DropColumn(
+                name: "Updated",
+                table: "TodoItems");
+        }
+    }
+}
--- backend/Entities/TodoItem.cs
+++ backend/Entities/TodoItem.cs
@@ -4,4 +4,15 @@ public class TodoItem
 {
     public int Id { get; set; }
     public required string Title { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime? Updated { get; set; }
+    public bool IsComplete { get; set; }
+    public bool IsDeleted { get; set; }
+
+    public TodoItem()
+    {
+        Created = DateTime.UtcNow;
+        IsComplete = false;
+        IsDeleted = false;
+    }
 }
