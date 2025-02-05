27058e86402a6b9d6d48b5c86d90807447fb7b73
stevenkleinberg <stevenkleinberg5@gmail.com>
2025-02-05 12:07:06 -0600

    restore deleted todo's working and tests passing

--- backend/DTOs/TodoDto.cs
+++ backend/DTOs/TodoDto.cs
@@ -5,7 +5,7 @@ public class TodoDto
 {
     public string? Title { get; set; }
     public DateTime? Updated { get; set; }
-    public bool IsComplete { get; set; } = false;
+    public bool? IsComplete { get; set; }
 
-    public bool IsDeleted { get; set; } = false;
+    public bool? IsDeleted { get; set;}
 }
