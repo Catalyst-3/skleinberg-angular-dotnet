baa8dd4d67bdb68cd72c99cc36ed83c9603b4cac
stevenkleinberg <stevenkleinberg5@gmail.com>
2025-02-06 12:08:15 -0600

    filter select added to todo list

--- frontend/src/app/todo-list/todo-list.component.ts
+++ frontend/src/app/todo-list/todo-list.component.ts
@@ -1,9 +1,8 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
-import { environment } from '../../environments/environment';
 import { TodoService } from '../_services/todo.service';
 import { Todo } from '../_models/todo';
 import { TodoCardComponent } from "../todo-card/todo-card.component";
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-todo-list',
@@ -13,22 +12,31 @@ import { TodoCardComponent } from "../todo-card/todo-card.component";
 })
 export class TodoListComponent implements OnInit {
   private todoService = inject(TodoService);
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+
   listName = "Your Current todo list";
   allTodos: Todo[] = [];
   visibleTodos: Todo[] = [];
-  
+  currentFilter: string = 'all';
+
   ngOnInit(): void {
-    this.loadTodos();
+    this.route.queryParams.subscribe(params => {
+      this.currentFilter = params['filter'] || 'all';
+      this.loadTodos();
+    });
+
     this.todoService.todoListUpdated.subscribe(() => {
       this.filterTodos();
     });
   }
 
-  loadTodos(){
+
+  loadTodos() {
     this.todoService.getAllTodos().subscribe({
       next: todos => {
         this.allTodos = todos;
-        this.filterTodos(); 
+        this.filterTodos();
       },
       error: error => console.log(error),
       complete: () => console.log('Request has completed')
@@ -36,6 +44,28 @@ export class TodoListComponent implements OnInit {
   }
 
   filterTodos() {
-    this.visibleTodos = this.allTodos.filter(todo => !todo.isDeleted);
+    switch (this.currentFilter) {
+      case 'deleted':
+        this.visibleTodos = this.allTodos.filter(todo => todo.isDeleted);
+        this.listName = "Deleted Todos";
+        break;
+      case 'completed':
+        this.visibleTodos = this.allTodos.filter(todo => todo.isComplete && !todo.isDeleted);
+        this.listName = "Completed Todos";
+        break;
+      case 'incomplete':
+        this.visibleTodos = this.allTodos.filter(todo => !todo.isComplete && !todo.isDeleted);
+        this.listName = "Incomplete Todos";
+        break;
+      default:
+        this.visibleTodos = this.allTodos.filter(todo => !todo.isDeleted);
+        this.listName = "Your Current Todo List";
+        break;
+    }
+  }
+
+  changeFilter(event: Event) {
+    const selectedFilter = (event.target as HTMLSelectElement).value;
+    this.router.navigate(['/todos'], { queryParams: { filter: selectedFilter } });
   }
 }
